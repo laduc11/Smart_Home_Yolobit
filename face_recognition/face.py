ee5f037d174abda137c3ce639fa9a3f96133e690
La Minh Duc <laminhduc200@gmail.com>
2024-05-15 22:13:27 +0700

    add open lock with face ID

--- face_recognition/face.py
+++ face_recognition/face.py
@@ -4,53 +4,55 @@ from keras.models import load_model  # TensorFlow is required for Keras to work 
 import cv2  # Install opencv-python
 import numpy as np
 from scipy import spatial
-import time
+# import time
 
 
-def recognition(s, embedding, threshold):
+def recognize(pic=None, threshold=0.9):
+    if pic is None:
+        return "unknown", False
+
+    gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray, 1.1, 4)
+    face = np.array(face)
+    if len(face) == 0:
+        return "unknown", False
+    face = face[np.argmax(face[:, 2])]
+    face = np.expand_dims(face, 0)
+    for (x, y, w, h) in face:
+        face = pic[y:y + h, x:x + w]
+        face = cv2.resize(face, (75, 100))
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+
+    embedding = embedding_model.predict(np.expand_dims(np.array(face), axis=0))
+    s = ""
+    with open('face_recognition/image_features.txt', "r") as file:
+        for line in file:
+            s += line
+
+    similar_face = []
+    # convert (string) s to (np.ndarray) vector
     for i in range(s.count(',')):
         string = s.split(',')[i]
         class_name = string[:string.index(' ')].replace("\n", "")
-        vector = np.fromstring(string[string.index(' ')+3:string.index(']]')].replace("\n", ""), dtype=float, sep=' ')
+        vector = np.fromstring(string[string.index(' ') + 3:string.index(']]')].replace("\n", ""), dtype=float, sep=' ')
         similarity = 1 - spatial.distance.cosine(embedding[0], vector)
-        print(similarity)
         if similarity >= threshold:
-            return class_name, True
-    return "false", False
+            similar_face.append((class_name, similarity))
+            print("class name: {}, similarity value: {}".format(class_name, similarity))
+
+    if len(similar_face) != 0:
+        sorted_similar_face = sorted(similar_face, key=lambda node: node[1], reverse=True)
+        return sorted_similar_face[0][0], True
+    return "unknown", False
 
 
 # Load the embedding_layer
-embedding_layer = load_model("embedding_layer.h5", compile=False)
+embedding_layer = load_model("face_recognition/embedding_layer.h5", compile=False)
 
 # Embedding model
-anchor_input = tf.keras.Input(shape=(100,75,3))
+anchor_input = tf.keras.Input(shape=(100, 75, 3))
 anchor_embeddings = embedding_layer(anchor_input)
 embedding_model = models.Model(inputs=anchor_input, outputs=anchor_embeddings)
 
 # Face detect with Haar Cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# start = time.time()
-pic = cv2.imread('./bach.jpg')  # modify with cv2 capture image
-
-gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
-face = face_cascade.detectMultiScale(gray, 1.1, 4)
-face = face[np.argmax(face[:, 2])]
-face = np.expand_dims(face, 0)
-for (x, y, w, h) in face:
-    face = pic[y:y + h, x:x + w]
-    face = cv2.resize(face, (75, 100))
-    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-
-embedding = embedding_model.predict(np.expand_dims(np.array(face), axis = 0))
-# print(f"embedding time: {time.time() - start}")
-s = ""
-with open('./image_features.txt', "r") as file:
-    for line in file:
-        s += line
-
-
-threshold = 0.9
-name, isTrue = recognition(s, embedding, threshold)
-print(name)
-# print(f"run time: {time.time() - start}")
+face_cascade = cv2.CascadeClassifier("face_recognition/haarcascade_frontalface_default.xml")
--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import threading
 import time
 import uart
 import database
+import cv2
+import face_recognition.face as model
+# import face_recognition.save_pic as update_data
 
 
 LOCK = threading.Lock()
-LOG_FILE = open('output.log', 'w')
 close_gateway = [False]
+turn_on_camera = [False]
 
 
-def receive_data_from_server():
+def receive_data_from_server(file_name):
     global close_gateway
     snapshot_database_changed = threading.Thread(target=database.database_changed)
     snapshot_database_changed.start()
@@ -22,18 +25,22 @@ def receive_data_from_server():
                 if temp[name] != database.prev_database[0][name]:
                     LOCK.acquire(blocking=True)
                     uart.write_data(name, database.prev_database[0][name])
-                    print("Data from server is updated", file=LOG_FILE)
-                    print(f"name: {name}\tvalue: {database.prev_database[0][name]}", file=LOG_FILE)
+                    with open(file_name, mode='a') as file:
+                        print("Data from server is updated", file=file)
+                        print(f"name: {name}\tvalue: {database.prev_database[0][name]}", file=file)
                     LOCK.release()
             database.is_database_changed[0] = False
 
 
-def receive_data_from_uart():
+def receive_data_from_uart(file_name):
     global close_gateway
     while not close_gateway[0]:
         data = uart.read_serial()
         for factor in data:
-            print(f"factor: {factor}", file=LOG_FILE)
+            LOCK.acquire(blocking=True)
+            with open(file_name, mode='a') as file:
+                print(f"factor: {factor}", file=file)
+            LOCK.release()
             database.update_dashboard(factor[0], factor[1])
             if factor[0] == 'open_door':
                 if factor[1] == '1':
@@ -42,11 +49,14 @@ def receive_data_from_uart():
                     database.log_activity('close door')
 
 
-def update_time_for_circuit():
+def update_time_for_circuit(file_name):
     global close_gateway
     while not close_gateway[0]:
         LOCK.acquire(blocking=True)
         uart.send_time()
+        # log activities to file
+        with open(file_name, mode='a') as file:
+            print("sending time", file=file)
         LOCK.release()
         time.sleep(30)
 
@@ -56,16 +66,49 @@ def send_password():
     while not close_gateway[0]:
         if not database.DASHBOARD.get()['open_door']:
             # door is close
-            # allow user type password to open it
-            password = input("Enter your password: ")
-            LOCK.acquire(blocking=True)
-            uart.write_data('password', password)
-            LOCK.release()
+            print("The door is closed. Choose the way to open the door")
+            print("1. Type the password")
+            print("2. Use face recognition")
+            option = input("Enter your option: ")
+            if option == 'close':
+                close_gateway[0] = True
+                continue
+
+            while option != '1' and option != '2':
+                print("Invalid option")
+                option = input("Enter your option: ")
+
+            if option == '1':
+                # allow user type password to open it
+                password = input("Enter your password: ")
+                LOCK.acquire(blocking=True)
+                uart.write_data('password', password)
+                LOCK.release()
+            elif option == '2':
+                # call function to detect face before camera
+                # return True if correct face else return False
+                # input: camera, timeout
+                # timeout: run until stop_flag is raised if timeout = -1
+                #          else run camera within timeout (s)
+
+                camera = cv2.VideoCapture(0)
+                while True:
+                    ret, frame = camera.read()
+                    cv2.imshow('camera', frame)
+                    name, is_known = model.recognize(pic=frame, threshold=0.1)
+                    if is_known:
+                        print(name)
+                        # break
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                camera.release()
+                cv2.destroyAllWindows()
         else:
             # door is open
             # allow user change the password or lock the door
-            print("You can change your password or close the door")
-            print("Type 1 to change your password and 2 to close the door")
+            print("The door is opened. Choose your option:")
+            print("1. Change your password")
+            print("2. Close the door")
             option = input("Enter your option: ")
             while option != '1' and option != '2':
                 print("Invalid option")
@@ -81,19 +124,22 @@ def send_password():
                 uart.write_data('open_door', '0')
                 database.update_dashboard('open_door', False)
                 LOCK.release()
-                pass
-            pass
+
+
+def open_door_with_face_recognition():
+    pass
 
 
 def main():
-    global LOG_FILE
     print("gateway is running")
+    # log activities to output.log
+    log_file = "output.log"
 
     uart.send_time()
 
-    thread_for_server = threading.Thread(target=receive_data_from_server)
-    thread_for_uart = threading.Thread(target=receive_data_from_uart)
-    thread_update_time = threading.Thread(target=update_time_for_circuit)
+    thread_for_server = threading.Thread(target=receive_data_from_server, args=(log_file,))
+    thread_for_uart = threading.Thread(target=receive_data_from_uart, args=(log_file,))
+    thread_update_time = threading.Thread(target=update_time_for_circuit, args=(log_file,))
     thread_for_password = threading.Thread(target=send_password)
 
     thread_for_server.start()
@@ -107,7 +153,6 @@ def main():
     thread_for_password.join()
 
     uart.SER.close()
-    LOG_FILE.close()
     print("end")
 
 
